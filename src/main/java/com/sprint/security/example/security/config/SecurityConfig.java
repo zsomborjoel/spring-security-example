8ab4b53850792e13322661fe28619fca0216aa0c
zsomborjoel <zsomborjoel@gmail.com>
2019-11-23 17:31:50 +0100

    Initial commit

---
+++ src/main/java/com/sprint/security/example/security/config/SecurityConfig.java
@@ -0,0 +1,112 @@
+package com.system.eas.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.signing-key}")
+    private String signingKey;
+
+    @Value("${security.encoding-strength}")
+    private Integer encodingStrength;
+
+    @Value("{security.security-realm}")
+    private String securityRealm;
+
+    /**
+     *   
+     * Verify if the authentication was valid
+     */
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    /**
+     * 
+     * AuthenticationManager is a config helper
+     * Local configuration (@Override)
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+            .withUser("spring")
+            .password(encoder.encode("secret"))
+            .roles("USER");        
+    }
+
+    /**
+     * 
+     * HttpSecurity for all http requests
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement() // allow configuring session management
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Spring Security will never create an HttpSession 
+            .and()
+            .httpBasic()
+            .realmName(securityRealm)
+            .and()
+            .csrf() // disabled becouse enabled default via spring security
+            .disable();
+    }
+
+    /**
+     * 
+     * Decode access tokens
+     */
+    @Bean 
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signingKey);
+        return converter;
+    }
+
+    /**
+     * 
+     * @return In Memory Token store
+     */
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    /**
+     * 
+     * @return defaultTokenServices
+     * which contains tokens
+     */
+    @Bean
+    @Primary //to avoid duplication with other token service
+    public DefaultTokenServices tokenService() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true); // refresh token = new valin access token
+        return defaultTokenServices;
+    }
+    
+    
+
+}
---
+++ src/test/java/com/sprint/security/example/security/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.security.example.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
